d7c5034a118b7df05a3ba0035faea224cb52bbf5
jack <jrjackrabbit2004@icloud.com>
2026-01-20 17:06:18 -0600

    handles dynamic array access and resizing. also handles new food creation, though this is subject to change.

---
+++ array_manip.c
@@ -0,0 +1,101 @@
+#include "types.h"
+#include "array_manip.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Meal create_meal(void){
+    
+    char buffer[256];
+    Meal hold;
+        
+    getchar();
+    
+    printf("\nenter the name of the meal: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    strncpy(hold.name, buffer, sizeof(hold.name) - 1);
+    hold.name[sizeof(hold.name) - 1] = '\0';
+        
+    buffer[0] = '\0'; 
+        
+    printf("\nenter the description of the meal: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    strncpy(hold.description, buffer, sizeof(hold.description) - 1);
+    hold.description[sizeof(hold.description) - 1] = '\0';
+        
+    buffer[0] = '\0';
+        
+    int type;
+    printf("\nwhat type of meal is this? (0 = main meal, 1 = side meal, 2 = snack, 3 = dessert, 4 = beverage)\n> ");
+    scanf("%d", &type);
+    while (type < 0 || type > 4) {
+        printf("invalid. Enter 0-4: ");
+        scanf("%d", &type);
+    }
+    hold.type = type;
+        
+    int cals;
+    printf("\nhow many calories are in this meal?: ");
+    scanf("%d", &cals);
+    hold.cals = cals;
+    
+    return hold;
+}
+
+void add_entry(Entries *entries){
+    
+    size_t new_appends;
+    printf("how many entries would you like to add?\n> ");
+    scanf("%zu", &new_appends);
+    
+    //check to see if array must be resized. does so if necessary, then adds new entries_array.
+    size_t required_capacity = entries->count + new_appends;
+    if(required_capacity > entries->capacity){
+        
+        while(entries->capacity < required_capacity){
+            
+            entries->capacity *= ARRAY_RESIZE_VALUE;
+        }
+        
+        Entry *temp = realloc(entries->entries_array, entries->capacity * sizeof(Entry));
+        if(!temp){
+            fprintf(stderr, "error: memory reallocation failed\n");
+            return;
+        }       
+        entries->entries_array = temp;
+    }
+    
+    for(size_t i = 0; i < new_appends; i++){
+    
+        Meal hold = create_meal();
+        
+        Entry entry;
+        entry.meal = hold;
+        entry.time = time(NULL);
+        entries->entries_array[entries->count+i] = entry; 
+    }
+    
+    entries->count += new_appends;
+}
+
+
+void print_array(Entries *entries){
+    
+    printf(" \nThe array currently has %zu elements:\n", entries->count);
+    for(size_t i = 0; i < entries->count; i++){
+        
+        time_t now = now = entries->entries_array[i].time;;
+        char *string_now = ctime(&now);        
+        
+        printf("\n%s: %d cals.\ndescription: %s\ntime: %s\n",
+        entries->entries_array[i].meal.name, 
+        entries->entries_array[i].meal.cals, 
+        entries->entries_array[i].meal.description,
+        string_now);
+    }
+    
+    printf("\n");
+}
---
+++ array_manip.h
@@ -0,0 +1,9 @@
+#ifndef ARRAY_MANIP_H
+#define ARRAY_MANIP_H
+
+
+Meal create_meal();
+void add_entry(Entries *entries);
+void print_array(Entries *entries);
+
+#endif
